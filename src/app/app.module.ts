7251524b94294933714b39caf784431b4166b755
gprohorov <george.prokhorov@external.getneteurope.com>
2022-03-08 23:12:58 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EmployeeService } from './employee/employee.service';
+import { EmployeeService } from './employee.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ItemComponent } from './item/item.component';
